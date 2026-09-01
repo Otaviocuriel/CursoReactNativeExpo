95da41a0cdf3a012f7796774056a1c810641c7ed
Otaviocuriel <otavio.curiel345@gmail.com>
2026-09-01 10:29:49 -0300

    feat: Async Storage

--- LinksAPP/src/App/_layout.tsx
+++ LinksAPP/src/App/_layout.tsx
@@ -7,6 +7,7 @@ export default function Layout() {
 
   return (
     <Stack
+      initialRouteName="index/index"
       screenOptions={{
         headerShown: false,
         contentStyle: { backgroundColor },
